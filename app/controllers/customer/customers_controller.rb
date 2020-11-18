9262480d1ab18fbc664803adfcbee1f36b8fdebe
EC2 Default User <ec2-user@ip-172-31-79-54.ec2.internal>
2020-11-18 08:01:13 +0000

    [update]customerコントローラとビューの作成

---
+++ app/controllers/customer/customers_controller.rb
@@ -0,0 +1,40 @@
+class CustomersController < ApplicationController
+  before_action :authenticate_customer!
+
+  def show
+    @customer = current_customer
+	end
+
+	def quit
+	end
+
+	def out
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+
+    reset_session
+    flash[:notice] = "ありがとうございました。またのご利用を心よりお待ちしております。"
+    redirect_to root_path
+	end
+
+	def edit
+    @customer = current_customer
+	end
+
+	def update
+    @customer = current_customer
+		if @customer.update(customer_params)
+       flash[:success] = "登録情報を変更しました"
+       redirect_to customers_path
+    else
+       render :edit and return
+    end
+	end
+
+	private
+
+	def customer_params
+  	params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postcode, :address, :telephone_number)
+  end
+
+end
