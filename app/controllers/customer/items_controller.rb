a99ea83f935097465c66d521be16501927b3be43
EC2 Default User <ec2-user@ip-172-31-79-54.ec2.internal>
2020-11-19 01:58:07 +0000

    [add]itemコントローラとビューの作成

---
+++ app/controllers/customer/items_controller.rb
@@ -0,0 +1,29 @@
+class Customer::ItemsController < ApplicationController
+  before_action :authenticate_customer!, only: [:show]
+
+  def top
+    @items = Item.all.order(created_at: :asc)
+    #=> :asc,古い順 :desc,新しい順
+    @genres = Genre.all
+	end
+
+	def index
+    @genres = Genre.all
+    @items = Item.where(is_active: true).page(params[:page]).per(8)
+	end
+
+	def show
+    @items = item.all
+    @item = item.find(params[:id])
+    @cart_item = CartItem.new
+	end
+
+  def about
+  end
+
+	private
+	def item_params
+		parmas.require(:item).permit(:image_id ,:name, :body, :price, :is_active)
+	end
+
+end
