afacaec0247d59912044d3351a3f79a113e6dd5d
EC2 Default User <ec2-user@ip-172-31-46-218.ec2.internal>
2020-11-19 03:42:23 +0000

    adminのorderのコントローラーの追記

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,39 @@
 class OrdersController < ApplicationController
+     before_action :authenticate_admin!
+
+  def index
+  		@orders = Order.all.page(params[:page]).per(10)
+  end
+
+  def current_index
+    @orders = Order.where(customer_id: params[:id]).page(params[:page]).per(10)
+    render :index
+  end
+
+  def today_order_index
+    now = Time.current
+    @orders = Order.where(created_at: now.all_day).page(params[:page]).per(10)
+    render :index
+  end
+
+	def show
+		@order = Order.find(params[:id])
+		@order_items = @order.order_items
+	end
+
+	def update
+		@order = Order.find(params[:id])
+		if @order.update(order_params)
+		   flash[:success] = "注文ステータスを変更しました"
+		   redirect_to admin_order_path(@order)
+		else
+		   render "show"
+		end
+	end
+
+	private
+	def order_params
+		  params.require(:order).permit(:status)
+	end
+
 end
