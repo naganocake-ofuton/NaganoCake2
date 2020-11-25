fd992dfe5d2635718c9411ee55100d62ae39e643
EC2 Default User <ec2-user@ip-172-31-79-54.ec2.internal>
2020-11-25 07:31:17 +0000

    [fix]管理者側注文履歴詳細画面の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     # ログイン時のパスを変更してる
     def after_sign_in_path_for(resource)
       if customer_signed_in?
-        customers_path(resource)
+        root_path
       else
         admin_top_path
       end
